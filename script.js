ed9683b224f34366020f2c3c3892e72b7b832790
Ngala Mac <macngala4@gmail.com>
2023-06-27 09:17:50 +0100

    Create variables in script.js file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const mainMenu = document.querySelector('.nav-bar');
+const openMenu = document.querySelector('.humburger');
+const closeMenu = document.querySelector('.close-menu');
+const menuItems = document.querySelectorAll('.nav-link');
 
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
 
 function close() {
   mainMenu.style.top = '-100%';
